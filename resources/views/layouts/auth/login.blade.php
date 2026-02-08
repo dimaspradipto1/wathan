5da2233ec193a7b3c565524f9a65cefc8ec53c50
dimas pradipto <dpradipto8@gmail.com>
2026-02-08 12:59:40 +0700

    refactor: refaktor login

--- resources/views/layouts/auth/login.blade.php
+++ resources/views/layouts/auth/login.blade.php
@@ -6,10 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - nahdlatul wathan</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('dashboard/assets/css/bootstrap.css')}}">
-    <link rel="stylesheet" href="{{asset('dashboard/assets/vendors/bootstrap-icons/bootstrap-icons.css')}}">
-    <link rel="stylesheet" href="{{asset('dashboard/assets/css/app.css')}}">
-    <link rel="stylesheet" href="{{asset('dashboard/assets/css/pages/auth.css')}}">
+    <link rel="stylesheet" href="{{ asset('dashboard/assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('dashboard/assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('dashboard/assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('dashboard/assets/css/pages/auth.css') }}">
+    <style>
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+            overflow: hidden;
+        }
+
+        #auth {
+            height: 100vh;
+            overflow: hidden;
+        }
+
+        #auth #auth-left {
+            padding: 2rem 5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        #auth #auth-right {
+            background: url("{{ asset('landingpage/img/bg-nwbatam.jpg') }}") !important;
+            background-size: cover !important;
+            background-position: center !important;
+            height: 100vh;
+        }
+
+        .auth-logo {
+            margin-bottom: 2rem !important;
+        }
+    </style>
 </head>
 
 <body>
@@ -19,40 +51,44 @@
             <div class="col-lg-5 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
-                        <a href="{{route('landingpage')}}"><img src="{{asset('dashboard/assets/images/logo/logo.png')}}" alt="Logo" style="width: 50px; height: 50px;"></a>
+                        <a href="{{ route('landingpage') }}"><img
+                                src="{{ asset('dashboard/assets/images/logo/logo.png') }}" alt="Logo"
+                                style="width: 50px; height: 50px;"></a>
                     </div>
                     <h2 class="">Log in.</h2>
                     <p class=" mb-5">Log in with your data that you entered during registration.</p>
 
-                    <form action="{{route('loginproses')}}" method="POST">
-                      @csrf
+                    <form action="{{ route('loginproses') }}" method="POST">
+                        @csrf
 
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="email" name="email" class="form-control form-control-xl" placeholder="email">
+                            <input type="email" name="email" class="form-control form-control-xl"
+                                placeholder="email">
                             <div class="form-control-icon">
                                 <i class="bi bi-envelope"></i>
                             </div>
                         </div>
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" name="password" id="password" class="form-control form-control-xl" placeholder="Password">
+                            <input type="password" name="password" id="password" class="form-control form-control-xl"
+                                placeholder="Password">
                             <div class="form-control-icon" onclick="togglePasswordVisibility()">
                                 <i id="togglePasswordIcon" class="bi bi-eye"></i>
                             </div>
                         </div>
-                        
+
                         <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
                     </form>
-                    <div class="text-center mt-5 fs-4">
-                        <p class="text-gray-600">Don't have an account? <a href="{{route('register')}}" class="font-bold">Sign up</a>.</p> 
-                        <p>
-                          <p class="text-gray-600"><i class="fa-solid fa-arrow-left"></i> <a href="{{route('landingpage')}}" class="font-bold">kembali ke homepage</a>.</p> 
-                        <p>
+                    <div class="text-center mt-4 fs-6">
+                        <p class="text-gray-600 mb-1">Don't have an account? <a href="{{ route('register') }}"
+                                class="font-bold">Sign up</a>.</p>
+                        <p class="text-gray-600"><i class="fa-solid fa-arrow-left"></i> <a
+                                href="{{ route('landingpage') }}" class="font-bold">kembali ke homepage</a>.</p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-7 d-none d-lg-block">
                 <div id="auth-right">
-                 
+
                 </div>
             </div>
         </div>
@@ -61,20 +97,20 @@
 
     <script src="https://kit.fontawesome.com/63b8672806.js" crossorigin="anonymous"></script>
     <script>
-    function togglePasswordVisibility() {
-        const passwordField = document.getElementById('password');
-        const togglePasswordIcon = document.getElementById('togglePasswordIcon');
-        if (passwordField.type === 'password') {
-            passwordField.type = 'text';
-            togglePasswordIcon.classList.remove('bi-eye');
-            togglePasswordIcon.classList.add('bi-eye-slash');
-        } else {
-            passwordField.type = 'password';
-            togglePasswordIcon.classList.remove('bi-eye-slash');
-            togglePasswordIcon.classList.add('bi-eye');
+        function togglePasswordVisibility() {
+            const passwordField = document.getElementById('password');
+            const togglePasswordIcon = document.getElementById('togglePasswordIcon');
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                togglePasswordIcon.classList.remove('bi-eye');
+                togglePasswordIcon.classList.add('bi-eye-slash');
+            } else {
+                passwordField.type = 'password';
+                togglePasswordIcon.classList.remove('bi-eye-slash');
+                togglePasswordIcon.classList.add('bi-eye');
+            }
         }
-    }
-</script>
+    </script>
 </body>
 
 </html>
