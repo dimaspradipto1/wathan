2e47026d0e5478ef912d152c58897c9ae62f5a54
dimas pradipto <dpradipto8@gmail.com>
2026-02-08 11:39:04 +0700

    refactor: refaktor index dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,11 @@ use App\Models\Fasilita;
 use Illuminate\Http\Request;
 use App\Models\PengurusPondok;
 
+use App\Models\Prestasiptq;
+use App\Models\Prestasitpq;
+use App\Models\PrestasiPtqTpq;
+use Carbon\Carbon;
+
 class DashboardController extends Controller
 {
     public function dashboard()
@@ -16,11 +21,25 @@ class DashboardController extends Controller
         $guru = Guru::count();
         $user = User::count();
         $fasilita = Fasilita::count();
+
+        // Get monthly achievements count for current year
+        $year = Carbon::now()->year;
+        $monthlyData = [];
+
+        for ($i = 1; $i <= 12; $i++) {
+            $countPtq = Prestasiptq::whereYear('tanggal', $year)->whereMonth('tanggal', $i)->count();
+            $countTpq = Prestasitpq::whereYear('tanggal', $year)->whereMonth('tanggal', $i)->count();
+            $countPtqTpq = PrestasiPtqTpq::whereYear('tanggal', $year)->whereMonth('tanggal', $i)->count();
+
+            $monthlyData[] = $countPtq + $countTpq + $countPtqTpq;
+        }
+
         return view('layouts.dashboard.index', compact([
             'pengurusPondok',
             'guru',
             'user',
-            'fasilita'
+            'fasilita',
+            'monthlyData'
         ]));
     }
 }
--- resources/views/layouts/dashboard/index.blade.php
+++ resources/views/layouts/dashboard/index.blade.php
@@ -1,96 +1,96 @@
 @extends('layouts.dashboard.template')
 
 @section('content')
-<div class="page-heading">
-  <h3>Dashboard</h3>
-</div>
+    <div class="page-heading">
+        <h3>Dashboard</h3>
+    </div>
 
-<div class="page-content">
-  <section class="row">
-      <div class="col-12 col-lg-9">
-          <div class="row">
-              <div class="col-6 col-lg-3 col-md-6">
-                  <div class="card">
-                      <div class="card-body px-3 py-4-5">
-                          <div class="row">
-                              <div class="col-md-4">
-                                  <div class="stats-icon purple">
-                                    <i class="iconly-boldProfile"></i>
-                                  </div>
-                              </div>
-                              <div class="col-md-8">
-                                  <h6 class="text-muted font-semibold">pengurus Pondok</h6>
-                                  <h6 class="font-extrabold mb-0">{{$pengurusPondok}}</h6>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-6 col-lg-3 col-md-6">
-                  <div class="card">
-                      <div class="card-body px-3 py-4-5">
-                          <div class="row">
-                              <div class="col-md-4">
-                                  <div class="stats-icon blue">
-                                      <i class="iconly-boldProfile"></i>
-                                  </div>
-                              </div>
-                              <div class="col-md-8">
-                                  <h6 class="text-muted font-semibold">Guru</h6>
-                                  <h6 class="font-extrabold mb-0">{{$guru}}</h6>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-6 col-lg-3 col-md-6">
-                  <div class="card">
-                      <div class="card-body px-3 py-4-5">
-                          <div class="row">
-                              <div class="col-md-4">
-                                  <div class="stats-icon green">
-                                      <i class="iconly-boldAdd-User"></i>
-                                  </div>
-                              </div>
-                              <div class="col-md-8">
-                                  <h6 class="text-muted font-semibold">pengguna</h6>
-                                  <h6 class="font-extrabold mb-0">{{$user}}</h6>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-6 col-lg-3 col-md-6">
-                  <div class="card">
-                      <div class="card-body px-3 py-4-5">
-                          <div class="row">
-                              <div class="col-md-4">
-                                  <div class="stats-icon red">
-                                      <i class="iconly-boldBookmark"></i>
-                                  </div>
-                              </div>
-                              <div class="col-md-8">
-                                  <h6 class="text-muted font-semibold">Fasilitas</h6>
-                                  <h6 class="font-extrabold mb-0">{{$fasilita}}</h6>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="row">
-              <div class="col-12">
-                  <div class="card">
-                      <div class="card-header">
-                          <h4>Profile Visit</h4>
-                      </div>
-                      <div class="card-body">
-                          <div id="chart-profile-visit"></div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="row">
+    <div class="page-content">
+        <section class="row">
+            <div class="col-12 col-lg-9">
+                <div class="row">
+                    <div class="col-6 col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-3 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="stats-icon purple">
+                                            <i class="iconly-boldProfile"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h6 class="text-muted font-semibold">pengurus Pondok</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $pengurusPondok }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-3 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="stats-icon blue">
+                                            <i class="iconly-boldProfile"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h6 class="text-muted font-semibold">Guru</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $guru }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-3 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="stats-icon green">
+                                            <i class="iconly-boldAdd-User"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h6 class="text-muted font-semibold">pengguna</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $user }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-3 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="stats-icon red">
+                                            <i class="iconly-boldBookmark"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h6 class="text-muted font-semibold">Fasilitas</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $fasilita }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Prestasi Siswa</h4>
+                            </div>
+                            <div class="card-body">
+                                <div id="chart-profile-visit"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="row">
               <div class="col-12 col-xl-4">
                   <div class="card">
                       <div class="card-header">
@@ -203,23 +203,23 @@
                       </div>
                   </div>
               </div>
-          </div>
-      </div>
-      <div class="col-12 col-lg-3">
-          <div class="card">
-              <div class="card-body py-4 px-5">
-                  <div class="d-flex align-items-center">
-                      <div class="avatar avatar-xl">
-                          <img src="{{asset('dashboard/assets/images/faces/1.jpg')}}" alt="Face 1">
-                      </div>
-                      <div class="ms-3 name">
-                          <h5 class="font-bold">{{auth()->user()->name}}</h5>
-                          <h6 class="text-muted mb-0">{{auth()->user()->email}}</h6>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="card">
+          </div> --}}
+            </div>
+            <div class="col-12 col-lg-3">
+                <div class="card">
+                    <div class="card-body py-4 px-5">
+                        <div class="d-flex align-items-center">
+                            <div class="avatar avatar-xl">
+                                <img src="{{ asset('dashboard/assets/images/faces/1.jpg') }}" alt="Face 1">
+                            </div>
+                            <div class="ms-3 name">
+                                <h5 class="font-bold">{{ auth()->user()->name }}</h5>
+                                <h6 class="text-muted mb-0">{{ auth()->user()->email }}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="card">
               <div class="card-header">
                   <h4>Recent Messages</h4>
               </div>
@@ -256,16 +256,48 @@
                           Conversation</button>
                   </div>
               </div>
-          </div>
-          <div class="card">
+          </div> --}}
+                {{-- <div class="card">
               <div class="card-header">
                   <h4>Visitors Profile</h4>
               </div>
               <div class="card-body">
                   <div id="chart-visitors-profile"></div>
               </div>
-          </div>
-      </div>
-  </section>
-</div>
+          </div> --}}
+            </div>
+        </section>
+    </div>
+    @push('script')
+        <script>
+            var optionsProfileVisit = {
+                annotations: {
+                    position: 'back'
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                chart: {
+                    type: 'bar',
+                    height: 300
+                },
+                fill: {
+                    opacity: 1
+                },
+                plotOptions: {},
+                series: [{
+                    name: 'Prestasi',
+                    data: {!! json_encode($monthlyData) !!}
+                }],
+                colors: '#435ebe',
+                xaxis: {
+                    categories: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agt", "Sep", "Okt", "Nov", "Des"],
+                },
+            }
+            var chartContainer = document.querySelector("#chart-profile-visit");
+            chartContainer.innerHTML = "";
+            var chartProfileVisit = new ApexCharts(chartContainer, optionsProfileVisit);
+            chartProfileVisit.render();
+        </script>
+    @endpush
 @endsection
